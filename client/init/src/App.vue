2e7685f3b33c6fb63de2c168cdb2c6ba9bb5ef2a
sisi-2665902516 <2665902516@qq.com>
2020-09-23 22:56:42 +0800

    9-23 yps update

--- client/init/src/App.vue
+++ client/init/src/App.vue
@@ -1,16 +1,8 @@
 <template>
-<<<<<<< HEAD
 	<div>
 		<topnav></topnav>
 		<router-view></router-view>
 	</div>
-=======
- <div>
-  <!-- <qiye></qiye> -->
- <topnav></topnav>
- <router-view></router-view>
- </div>
->>>>>>> 2753c08e54d172d1541620d74b348b220f2bb4b5
 </template>
 
 <script>
@@ -22,22 +14,7 @@
 			topnav,
 			goodmodule
 
-<<<<<<< HEAD
 		}}
-=======
-// import qiye from '@/views/qiye.vue'
-import topnav from "./components/wang-component/topnav.vue"
-import goodmodule from "./components/wang-component/goodmodule.vue"
-export default { 
-  name: 'App',
-  components: {
-	topnav,
-    // qiye,
-	topnav,
-	goodmodule
-  }
-}
->>>>>>> 2753c08e54d172d1541620d74b348b220f2bb4b5
 </script>
 
 <style>
